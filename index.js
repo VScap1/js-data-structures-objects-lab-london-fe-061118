30895faa714725b2b01e204c556ae1a05f66e68d
vanessa_scappaticci@hotmail.co.uk <vanessa_scappaticci@hotmail.co.uk>
2018-07-18 19:44:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,19 @@ letdriver=(
   
   )
 UpdateDriverWithKeyAndValue()
+
+const obj = { foo: 'bar' };
+ 
+const newObj = Object.assign({}, obj);
+ 
+newObj;
+// => { foo: "bar" }
+ 
+delete newObj.foo;
+// => true
+ 
+newObj;
+// => {}
+ 
+obj;
+// => { foo: "bar" }
